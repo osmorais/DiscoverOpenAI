b237f34626d446b658832bb5eeb3b86d024c4ebe
-osmar <osmar.junior@marketup.com>
2024-01-30 02:16:15 -0300

    update voice and image tests

--- image.py
+++ image.py
@@ -6,10 +6,11 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 response = client.images.generate(
   model="dall-e-3",
-  prompt="a white siamese cat",
+  prompt="medieval bard playing heavy metal",
   size="1024x1024",
   quality="standard",
   n=1,
 )
 
 image_url = response.data[0].url
+print(image_url)
--- voz.py
+++ voz.py
@@ -5,15 +5,28 @@ client = OpenAI()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-response = client.audio.speech.create(
-    model="tts-1",
-    voice="onyx",
-    input="""
+message = """
+        Sou uma voz gerada por IA, e recitarei um poema:
+        
         De tudo, ao meu amor serei atento
         Antes, e com tal zelo, e sempre, e tanto
         Que mesmo em face do maior encanto
         Dele se encante mais meu pensamento.
-    """,
+    """
+
+
+response = client.audio.speech.create(
+    model="tts-1",
+    voice="nova",
+    input=message,
 )
+
+#Voices:
+# Alloy
+# Echo
+# Fable
+# Onyx
+# Nova
+# Shimmer
 
 response.stream_to_file("output.mp3")
